ddb32a26c1ae491293c89d01f485ce8094bf08b8
Muhammad Umar <muhammadumar695@gmail.com>
2024-04-05 17:20:57 +0500

    The top bar with coins details and user details is created and added successfully. Additionally, the refresh button is also added. Further, moving on to the main NFT components designing

--- Artistic NFT Hub/src/components/main/Main.jsx
+++ Artistic NFT Hub/src/components/main/Main.jsx
@@ -2,12 +2,67 @@
 import React from 'react'
 import '../../styles/main.css'
 
+import Unit from '../../components/units/Unit'
+
+import Eicon from '../../assets/Eicon.png'
+
+import Refresh from  '../../assets/refresh.png'
+import Avatar from  '../../assets/avatar.png'
+
 const Main = () => {
 
     return (
         <>
             <div className='main' >
-                <h1> M </h1>
+                
+                <div className='main-first-div' >
+                    
+                    <div className='main-first-div-1' >
+                        
+                        <div className='main-first-div-1-left' >
+                            <div className='main-first-div-1-logo' >
+                                <img src={Eicon} alt = "ETH" />
+                            </div>
+                            <div className='main-first-div-1-eth' >
+                                ETH/USDT
+                            </div>
+                            <div className='main-first-div-1-money' >
+                                1137.62 + 2.62%↑
+                            </div>
+                        </div>
+
+                        <div className='main-first-div-1-right' >
+                            <div className='main-first-div-1-logo' >
+                                <img src={Avatar} alt = "Avatar" />
+                            </div>
+                            <div className='main-first-div-1-eth' >
+                                Zash
+                            </div>
+                            <div className='main-first-div-1-hr' >
+                                |
+                            </div>
+                            <div className='main-first-div-1-id' >
+                                User ID: 11026666
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='main-first-div-2' >
+                        <div>
+                            <div style = {{ fontSize : 'xx-large' }} > Dashboard </div>
+                        </div>
+                        <div style = {{ display : 'flex' , alignItems : 'center' }} >
+                            <img src={Refresh} alt="Refresh" />
+                        </div>
+                    </div>
+
+                    <div className='main-first-div-3' >
+                        3
+                        < Unit />
+                    </div>
+
+                </div>
+
             </div>
         </>
     )
---
+++ Artistic NFT Hub/src/components/units/Unit.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import '../../styles/unit.css';
+
+const Unit = () => {
+    
+    return (
+
+        <>
+            <div>
+                {/* The slidding NTS */}
+                <div>
+
+                </div>
+
+                {/* Buttons */}
+                <div>
+
+                </div>
+
+            </div>
+            
+        </>
+    )
+}
+
+export default Unit
