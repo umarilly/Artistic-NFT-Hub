997929b8e7a5cc32a65330765aaa08489fae1ad3
Muhammad Umar <muhammadumar695@gmail.com>
2024-04-05 18:01:28 +0500

    The structure for the sliders of NFT is created. A three box structure is set, on the click on next button, whole div position will move

--- Artistic NFT Hub/src/components/main/Main.jsx
+++ Artistic NFT Hub/src/components/main/Main.jsx
@@ -6,22 +6,22 @@ import Unit from '../../components/units/Unit'
 
 import Eicon from '../../assets/Eicon.png'
 
-import Refresh from  '../../assets/refresh.png'
-import Avatar from  '../../assets/avatar.png'
+import Refresh from '../../assets/refresh.png'
+import Avatar from '../../assets/avatar.png'
 
 const Main = () => {
 
     return (
         <>
             <div className='main' >
-                
+
                 <div className='main-first-div' >
-                    
+
                     <div className='main-first-div-1' >
-                        
+
                         <div className='main-first-div-1-left' >
                             <div className='main-first-div-1-logo' >
-                                <img src={Eicon} alt = "ETH" />
+                                <img src={Eicon} alt="ETH" />
                             </div>
                             <div className='main-first-div-1-eth' >
                                 ETH/USDT
@@ -33,7 +33,7 @@ const Main = () => {
 
                         <div className='main-first-div-1-right' >
                             <div className='main-first-div-1-logo' >
-                                <img src={Avatar} alt = "Avatar" />
+                                <img src={Avatar} alt="Avatar" />
                             </div>
                             <div className='main-first-div-1-eth' >
                                 Zash
@@ -49,18 +49,19 @@ const Main = () => {
 
                     <div className='main-first-div-2' >
                         <div>
-                            <div style = {{ fontSize : 'xx-large' }} > Dashboard </div>
+                            <div style={{ fontSize: 'xx-large' }} > Dashboard </div>
                         </div>
-                        <div style = {{ display : 'flex' , alignItems : 'center' }} >
+                        <div style={{ display: 'flex', alignItems: 'center' }} >
                             <img src={Refresh} alt="Refresh" />
                         </div>
                     </div>
 
+                </div>
+
+                <div className='main-second-div' >
                     <div className='main-first-div-3' >
-                        3
                         < Unit />
                     </div>
-
                 </div>
 
             </div>
--- Artistic NFT Hub/src/components/units/Unit.jsx
+++ Artistic NFT Hub/src/components/units/Unit.jsx
@@ -3,23 +3,37 @@ import React from 'react';
 import '../../styles/unit.css';
 
 const Unit = () => {
-    
+
     return (
 
         <>
-            <div>
+            <div className='main-units-area' >
+                
                 {/* The slidding NTS */}
-                <div>
+
+                <div className='main-units-area-slider' >
+
+                    <div className='main-first-slider-pos' >
+                        1
+                    </div>
+                    <div className='main-second-slider-pos' >
+                        2
+                    </div>
+                    <div className='main-third-slider-pos'>
+                        3
+                    </div>
 
                 </div>
 
+
                 {/* Buttons */}
+                
                 <div>
 
                 </div>
 
             </div>
-            
+
         </>
     )
 }
